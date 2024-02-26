e59f1bde652b2dcfa6091aedb6fe74acf2232bd3
pcaboor <pierre.caboor59@gmail.com>
2024-02-26 15:44:59 +0100

    \n

--- myfamily.sh
+++ myfamily.sh
@@ -1,3 +1,1 @@
-HERO_ID=1 
-
-curl -s "https://zone01normandie.org/assets/superhero/all.json" | jq -r --arg id "$HERO_ID" '.[] | select(.id==$id) | .connections.relatives | gsub("\""; "")'
+curl --silent "https://zone01normandie.org/assets/superhero/all.json" | jq -r --argjson hero_id $HERO_ID '.[] | select(.id == $hero_id) | .connections.relatives | gsub("\n"; "\n")'
