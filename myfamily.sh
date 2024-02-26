f3cf09feacd57f2628f85af313a906578a019392
pcaboor <pierre.caboor59@gmail.com>
2024-02-26 15:30:39 +0100

    Structure change

--- myfamily.sh
+++ myfamily.sh
@@ -1,6 +1,3 @@
-HERO_ID=$HERO_ID
+HERO_ID=1 
 
-curl -s "https://zone01normandie.org/assets/superhero/all.json" | jq  -r '.[] | select(.id==1) | .connections.relatives | gsub("\""; "")'
-
-
-
+curl -s "https://zone01normandie.org/assets/superhero/all.json" | jq -r --arg id "$HERO_ID" '.[] | select(.id==$id) | .connections.relatives | gsub("\""; "")'
