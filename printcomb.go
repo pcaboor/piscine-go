dcf5f782f1e4280385f4ee0102dfc9f5af9f60c0
root <dev@01-edu.org>
2024-02-28 11:16:35 +0000

    Add 'printcomb.go'

---
+++ printcomb.go
@@ -0,0 +1,18 @@
+package piscine
+
+import("github.com/01-edu/z01")
+
+// boucle forte
+
+func PrintComb(){	
+	for x := '0'; x<= '9'; x++ {
+      for y := x+1; y<= '9'; y++ {
+         for z := y+1; z<= '9'; z++ {
+		z01.PrintRune(x) // afficher un nombre de 0 à 9
+        z01.PrintRune(y) // afficher un nombre de 0 à 9
+        z01.PrintRune(z) // afficher un nombre de 0 à 9    
+        z01.PrintRune(44) // afficher des virgules
+        z01.PrintRune(32) // afficher des espace 
+	}}}
+	    z01.PrintRune('\n') // afficher en ligne 
+}
