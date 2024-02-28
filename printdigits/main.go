63af4a6e87bc5079a66c7bb11e25ff6f2499bc0b
pcaboor <pierre.caboor59@gmail.com>
2024-02-28 10:13:08 +0100

    learn ascii tab

---
+++ printdigits/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	var numOne rune
+	for numOne = 48; numOne <= 57; numOne++ {
+		z01.PrintRune(numOne)
+	}
+	z01.PrintRune('\n')
+}
