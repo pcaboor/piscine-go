8c57ca7724e3c312afdff1f8ed12ad6787b8305d
pcaboor <pierre.caboor59@gmail.com>
2024-02-28 09:54:37 +0100

    exercice 2 reverse alphabet

---
+++ printreversealphabet/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	var rRune rune
+	for rRune = 122; rRune >= 97; rRune-- {
+		z01.PrintRune(rRune)
+	}
+	z01.PrintRune('\n')
+
+}
