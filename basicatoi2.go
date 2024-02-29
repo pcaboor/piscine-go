366660f9125187a9bed42787aaa6def3c055e77e
pcaboor <pierre.caboor59@gmail.com>
2024-02-29 15:50:48 +0100

    add condition if 0to9

--- basicatoi2.go
+++ basicatoi2.go
@@ -3,8 +3,11 @@ package piscine
 func BasicAtoi2(s string) int {
 	myResult := 0
 	for _, numb := range s {
-		myResult = myResult*10 +
-			int(numb-'0')
+
+		if numb < '0' || numb > '9' {
+			return 0
+		}
+		myResult = myResult*10 + int(numb-'0')
 	}
 	return myResult
 }
