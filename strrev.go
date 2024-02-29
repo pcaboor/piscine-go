0d4819bfcbe463f400c7c13404cca621e8d2a1e8
pcaboor <pierre.caboor59@gmail.com>
2024-02-29 14:19:45 +0100

    x by y

--- strrev.go
+++ strrev.go
@@ -2,7 +2,7 @@ package piscine
 
 func StrRev(s string) string {
 	reverse := []rune(s)
-	for x, y := 0, len(reverse)-1; x < y; x, y = x+1, x-1 {
+	for x, y := 0, len(reverse)-1; x < y; x, y = x+1, y-1 {
 		reverse[x], reverse[y] = reverse[y], reverse[x]
 	}
 	return string(reverse)
