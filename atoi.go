0eefc09cfc0a5286a4e41cf8d65aec758e0da56d
root <dev@01-edu.org>
2024-03-01 09:44:48 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -1,21 +1,21 @@
 package piscine
-
-func Atoi(s string) int {
-myResult := 0 
-ponc := 1
-
-for a, b := range s {
-    if a == 0 && (b == '+' || b == '-') {
-        if b == '-' {
-           ponc := -1
-       }
-} else if b < '0' || b > '9' {
-    return 0
-    } else {
-      
-      myResult = myResult*10 + int(b -'0')
-    }  
   
-  } 
-  return myResult * ponc
-}
+func Atoi(s string) int {
+      myResult := 0 
+      ponc := 1
+  
+for a, b := range s {
+      if a == 0 && (b == '+' || b == '-') {
+          if b == '-' {
+               ponc = -1
+           }
+      } else if b < '0' || b > '9' {
+        return 0
+      } else {
+        
+         myResult = myResult*10 + int(b-'0')
+      }  
+    
+    } 
+    return myResult * ponc
+  }
