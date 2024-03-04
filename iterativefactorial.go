7f39f9343968254277768ac39773505ee20c7f3a
pcaboor <pierre.caboor59@gmail.com>
2024-03-04 09:49:05 +0100

    nb > 12

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,11 +4,11 @@ func IterativeFactorial(nb int) int {
 	result := 0
 
 	for i := 0; i <= nb+1; i++ {
+		if nb < 0 || nb > 12 {
+			return 0
+		}
 		if nb == 0 || nb == 1 {
 			return 1
-		}
-		if nb < 0 || nb <= 13 {
-			return 0
 		} else {
 			result = result + nb
 		}
