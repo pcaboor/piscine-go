dddd57a7a59359ff5927937c86a7cffa684d016d
pcaboor <pierre.caboor59@gmail.com>
2024-03-04 10:37:49 +0100

    space removed

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,7 +1,6 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-
 	if nb < 0 || nb > 20 {
 		return 0
 	}
@@ -9,7 +8,6 @@ func RecursiveFactorial(nb int) int {
 		return 1
 	}
 	for i := 1; i <= nb; i++ {
-
 	}
 	return nb * RecursiveFactorial(nb-1)
 }
