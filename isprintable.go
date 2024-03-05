5b450e663f75122ac55d80877ff8e3c502986b88
pcaboor <pierre.caboor59@gmail.com>
2024-03-05 14:12:46 +0100

    is printable

--- isprintable.go
+++ isprintable.go
@@ -1,10 +1,21 @@
 package piscine
 
 func IsPrintable(s string) bool {
-	for _, imp := range s {
-		if imp == '\n' {
+	for _, goodLetter := range s {
+		if goodLetter < ' ' || goodLetter > '~' {
 			return false
 		}
 	}
 	return true
 }
+
+/* func IsPrintable(s string) bool {
+	var aRune rune
+	for _, goodLetter := range s {
+		if goodLetter < 32 || goodLetter > 127 {
+			return false
+		}
+	}
+	return true
+}
+*/
