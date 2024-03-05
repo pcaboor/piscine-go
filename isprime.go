18e64b2fb933f1bcbe8d40712a3da1ce87931013
pcaboor <pierre.caboor59@gmail.com>
2024-03-05 10:13:43 +0100

    len

--- isprime.go
+++ isprime.go
@@ -4,8 +4,16 @@ func IsPrime(nb int) bool {
 	if nb == 1 {
 		return false
 	}
+	if nb == 0 {
+		return false
+	}
 	if nb%2 != 0 {
 		return true
 	}
+	if nb < 0 || nb%2 != 0 {
+		return false
+	}
 	return false
 }
+
+// nombre entier nb divisble par 1 et par lui même
---
+++ nrune.go
@@ -0,0 +1,9 @@
+package piscine
+
+func NRune(s string, n int) rune {
+	if n < 0 || n > len(s) {
+		return 0
+	}
+	temp := []rune(s)
+	return temp[n-1]
+}
