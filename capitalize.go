7a07bf9e515a1df94e1cceffc8b487a972a52ac5
pcaboor <pierre.caboor59@gmail.com>
2024-03-06 11:45:45 +0100

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,10 +1,22 @@
 package piscine
 
 func Capitalize(s string) string {
+	if len(s) <= 0 {
+		return ""
+	}
 	temp := []rune(s)
 	for i := 0; i < len(temp); i++ {
-		if temp[0] >= 'a' && temp[0] <= 'z' {
-			temp[0] -= 32
+		if temp[i] >= 'A' && temp[i] <= 'Z' || temp[i] >= 'a' && temp[i] <= 'z' || temp[i] >= '0' && temp[i] <= '9' {
+			if temp[i] >= 'a' && temp[i] <= 'z' {
+				temp[i] -= 32
+			}
+			i++
+			for i < len(temp) && temp[i] >= 'a' && temp[i] <= 'z' || temp[i] >= 'A' && temp[i] <= 'Z' || temp[i] >= '0' && temp[i] <= '9' {
+				if temp[i] >= 'A' && temp[i] <= 'Z' {
+					temp[i] += 32
+				}
+				i++
+			}
 		}
 	}
 	return string(temp)
