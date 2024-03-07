505d3f8d6a5178275730ece34c446e095e37a3fa
pcaboor <pierre.caboor59@gmail.com>
2024-03-07 13:27:01 +0100

    split

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,15 +4,17 @@ func SplitWhiteSpaces(s string) []string {
 	var tbstr []string
 	result := -1
 	for i, letter := range s {
-		if letter == ' ' || letter == '\t' || letter == '\n' {
+		if letter == ' ' || letter == '\n' || letter == '\t' {
 			if len(s[result+1:i]) > 0 {
-				if s[result+1:i] != " " {
+				if s[result+1:i] != "" {
 					tbstr = append(tbstr, s[result+1:i])
 				}
 				result = i
 			}
 		}
 	}
-	tbstr = append(tbstr, s[result+1:])
+	if result != len(s)-1 {
+		tbstr = append(tbstr, s[result+1:])
+	}
 	return tbstr
 }
