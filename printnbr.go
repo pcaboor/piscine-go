65caaba447471553fc971c3664e76ad022e225bf
pcaboor <pierre.caboor59@gmail.com>
2024-03-11 18:51:03 +0100

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -3,22 +3,12 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
-	if n == 0 {
-		z01.PrintRune('0')
-		return
-	}
 	if n < 0 {
+		n *= -1
 		z01.PrintRune('-')
-		n = -n
 	}
-	var tab [20]int
-	index := 0
-	for n > 0 {
-		tab[index] = n % 10
-		n /= 10
-		index++
+	if n >= 10 {
+		PrintNbr(n / 10)
 	}
-	for r := index - 1; r >= 0; r-- {
-		z01.PrintRune(rune('0' + tab[r]))
-	}
+	z01.PrintRune(rune('0' + n%10))
 }
