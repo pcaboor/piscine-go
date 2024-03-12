ff5b06d3f679f3a76098ea024a1c437d428f309a
pcaboor <pierre.caboor59@gmail.com>
2024-03-12 15:11:17 +0100

    doop

--- doop/main.go
+++ doop/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -30,21 +30,20 @@ func main() {
 
 	case "/":
 		if y == 0 {
-			fmt.Println("No division by 0")
+			os.Stderr.WriteString("No divisible by 0")
 			return
 		}
 		r = x / y
 	case "%":
 		if y == 0 {
-			fmt.Println("No modulo by 0")
+			os.Stderr.WriteString("No modulo by 0")
 			return
 		}
 		r = x % y
 	default:
 		return
 	}
-
-	fmt.Println(r)
+	os.Stdout.WriteString(its(r) + "\n")
 }
 
 func Atoi2(s string) (int, error) {
@@ -57,10 +56,31 @@ func Atoi2(s string) (int, error) {
 				ponc = -1
 			}
 		} else if b < '0' || b > '9' {
-			return 0, fmt.Errorf("Error")
+			return 0, errChr
 		} else {
 			myResult = myResult*10 + int(b-'0')
 		}
 	}
 	return myResult * ponc, nil
 }
+
+func its(i int) string {
+	if i == 0 {
+		return "0"
+	}
+	var r string
+	sign := ""
+
+	if i < 0 {
+		sign = "-"
+		i = -i
+	}
+	for i > 0 {
+		numb := i % 10
+		r = string(byte(numb)+'0') + r
+		i /= 10
+	}
+	return sign + r
+}
+
+var errChr = &strconv.NumError{Func: "Atoi", Num: "", Err: strconv.ErrSyntax}
