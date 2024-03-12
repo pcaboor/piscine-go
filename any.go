d358297f6b2ae499de63973005123b40b17bf1c7
pcaboor <pierre.caboor59@gmail.com>
2024-03-12 10:09:38 +0100

    any

---
+++ any.go
@@ -0,0 +1,21 @@
+package piscine
+
+func Any(f func(string) bool, a []string) bool {
+
+	for _, n := range a {
+		if f(n) {
+			// vérifie si la chaine est composée de chiffres
+			return true
+		}
+	}
+	return false
+}
+
+func IsNumericNumb(s string) bool {
+	for _, goodLetter := range s {
+		if goodLetter < '0' || goodLetter > '9' {
+			return false
+		}
+	}
+	return true
+}
