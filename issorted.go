89843f64ffc7a764843d3b238fc1e123464f245c
pcaboor <pierre.caboor59@gmail.com>
2024-03-12 11:05:43 +0100

    issorted

---
+++ issorted.go
@@ -0,0 +1,14 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	for j := 0; j < len(a)-1; j++ {
+		if f(a[j], a[j+1]) > 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func f(a, b int) int {
+	return a - b
+}
--- test_go/main.go
+++ test_go/main.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	tab1 := []string{"Hello", "how", "are", "you"}
 	tab2 := []string{"This", "1", "is", "4", "you"}
-	answer1 := piscine.CountIf(piscine.IsNumericNumb2, tab1)
-	answer2 := piscine.CountIf(piscine.IsNumericNumb2, tab2)
+	answer1 := piscine.CountIf(piscine.IsNumeric, tab1)
+	answer2 := piscine.CountIf(piscine.IsNumeric, tab2)
 	fmt.Println(answer1)
 	fmt.Println(answer2)
 }
