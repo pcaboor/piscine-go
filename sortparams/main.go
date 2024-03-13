f8fb3ed1412a5a95043928c41bc533262dc6d15c
pcabor <pierre.caboor59@gmail.com>
2024-03-13 11:47:19 +0100

    sortparams

---
+++ sortparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:] // Exclure le nom du programme
+	for i := 0; i <= 127; i++ {
+		for _, arg := range args {
+			if len(arg) > 0 && int(arg[0]) == i {
+				fmt.Println(arg)
+			}
+		}
+	}
+
+}
