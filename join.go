e3a6abb1715c4a5cb5e1aae93efcfdbd7ae90b30
pcabor <pierre.caboor59@gmail.com>
2024-03-13 11:17:21 +0100

    join

--- join.go
+++ join.go
@@ -3,7 +3,7 @@ package piscine
 func Join(strs []string, sep string) string {
 	res := ""                   // on défini une chaine de caractères vide qui va stocker les mots concat
 	for i, word := range strs { // on parcours tout le tableau du début à la fin
-		res += word // on ajoute les mots ensembles
+		res += word
 		if i != len(strs)-1 {
 			res += ":"
 		}
