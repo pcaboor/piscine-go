f5eb60a0ec8d51f29aa5467605d15402253a8af1
pcabor <pierre.caboor59@gmail.com>
2024-03-14 17:12:39 +0100

    enigma

---
+++ enigma.go
@@ -0,0 +1,7 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	***a, *b = *b, ***a
+	****d, *b = *b, ****d
+	****d, *******c = *******c, ****d
+}
--- test_go/main.go
+++ test_go/main.go
@@ -6,6 +6,40 @@ import (
 )
 
 func main() {
-	steps := piscine.CollatzCountdown(12)
-	fmt.Println(steps)
+	x := 5
+	y := &x
+	z := &y
+	a := &z
+
+	w := 2
+	b := &w
+
+	u := 7
+	e := &u
+	f := &e
+	g := &f
+	h := &g
+	i := &h
+	j := &i
+	c := &j
+
+	k := 6
+	l := &k
+	m := &l
+	n := &m
+	d := &n
+
+	fmt.Println(***a)
+	fmt.Println(*b)
+	fmt.Println(*******c)
+	fmt.Println(****d)
+
+	piscine.Enigma(a, b, c, d)
+
+	fmt.Println("After using Enigma")
+	fmt.Println(***a)
+	fmt.Println(*b)
+	fmt.Println(*******c)
+	fmt.Println(****d)
+
 }
