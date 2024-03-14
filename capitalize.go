1c596349edd69635283337be5c5c6a8f6084d246
root <dev@01-edu.org>
2024-03-14 15:11:30 +0000

    Update 'capitalize.go'

--- capitalize.go
+++ capitalize.go
@@ -6,12 +6,12 @@ func Capitalize(s string) string {
 	}
 	temp := []rune(s)
 	for i := 0; i < len(temp); i++ {
-		if temp[i] >= 'A' && temp[i] <= 'Z' || temp[i] >= 'a' && temp[i] <= 'z' || temp[i] >= '0' && temp[i] <= '9' {
+		if temp[i] >= 'A' && temp[i] <= 'z'  || temp[i] >= '0' && temp[i] <= '9' {
 			if temp[i] >= 'a' && temp[i] <= 'z' {
 				temp[i] -= 32
 			}
 			i++
-			for i < len(temp) && (temp[i] >= 'a' && temp[i] <= 'z' || temp[i] >= 'A' && temp[i] <= 'Z' || temp[i] >= '0' && temp[i] <= '9') {
+			for i < len(temp) && (temp[i] >= 'A' && temp[i] <= 'z' || temp[i] >= '0' && temp[i] <= '9') {
 				if temp[i] >= 'A' && temp[i] <= 'Z' {
 					temp[i] += 32
 				}
