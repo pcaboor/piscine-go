9fe1b18844a9e07aeb41febc52ecd951936c48db
pcabor <pierre.caboor59@gmail.com>
2024-03-14 15:08:02 +0100

    comcheck

---
+++ comcheck/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	a := os.Args[1:]
+
+	for _, ch := range a {
+		if ch == "01" || ch == "galaxy" || ch == "galaxy 01" {
+			fmt.Println("Alert!!!")
+		}
+	}
+}
