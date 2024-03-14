4cf0e8f28beff285eed8d129d8a9ba70d41a1693
pcabor <pierre.caboor59@gmail.com>
2024-03-14 13:21:44 +0100

    collatzcountdown

---
+++ collatzcountdown.go
@@ -0,0 +1,16 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	if start <= 0 {
+		return -1
+	}
+	count := 0
+	for ; start > 1; count++ {
+		if start%2 == 0 {
+			start = start / 2
+		} else {
+			start = 3*start + 1
+		}
+	}
+	return count
+}
--- test_go/main.go
+++ test_go/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	middle := piscine.Abort(2, 3, 8, 5, 7)
-	fmt.Println(middle)
+	steps := piscine.CollatzCountdown(12)
+	fmt.Println(steps)
 }
