35cb9abcef3b069f325b0a92d28103cd083760f0
pcabor <pierre.caboor59@gmail.com>
2024-03-15 05:15:34 +0100

    changed

--- unmatch.go
+++ unmatch.go
@@ -5,10 +5,14 @@ func Unmatch(a []int) int {
 	for _, n := range a {
 		t[n]++
 	}
+	t2 := []int{}
 	for n, count := range t {
 		if count%2 != 0 {
-			return n
+			t2 = append(t2, n)
 		}
+	}
+	if len(t2) == 1 {
+		return t2[0]
 	}
 	return -1
 }
