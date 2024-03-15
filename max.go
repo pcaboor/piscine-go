81b90e345c78fd39825f821c99c51a88d8783394
pcabor <pierre.caboor59@gmail.com>
2024-03-15 05:09:26 +0100

    match

--- max.go
+++ max.go
@@ -1,14 +1,14 @@
 package piscine
 
 func Max(a []int) int {
-  max := a[0]
-if len(a) <= 0 {
-    return 0
-  }
-for _,n := range a {
-if n > max {
-  max = n
-}
-}
-return max
+	max := a[0]
+	if len(a) <= 0 {
+		return 0
+	}
+	for _, n := range a {
+		if n > max {
+			max = n
+		}
+	}
+	return max
 }
