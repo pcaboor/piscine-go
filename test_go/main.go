9421f9c126692febe96da8bb5d69cf0279b4cb95
pcabor <pierre.caboor59@gmail.com>
2024-03-18 10:35:29 +0100

    space removed

--- test_go/main.go
+++ test_go/main.go
@@ -2,18 +2,14 @@ package main
 
 import (
 	"fmt"
-
 	"piscine"
 )
 
 func main() {
-
 	link := &piscine.List{}
-
 	piscine.ListPushFront(link, "Hello")
 	piscine.ListPushFront(link, "man")
 	piscine.ListPushFront(link, "how are you")
-
 	it := link.Head
 	for it != nil {
 		fmt.Print(it.Data, " ")
