b95f2379b5b0290a956260addb24d7d1b508cb92
pcabor <pierre.caboor59@gmail.com>
2024-03-18 12:03:07 +0100

    space removed

--- test_go/main.go
+++ test_go/main.go
@@ -7,12 +7,10 @@ import (
 
 func main() {
 	link := &piscine.List{}
-
 	piscine.ListPushBack(link, 1)
 	piscine.ListPushBack(link, 2)
 	piscine.ListPushBack(link, 3)
 	piscine.ListPushBack(link, 4)
-
 	piscine.ListReverse(link)
 
 	it := link.Head
