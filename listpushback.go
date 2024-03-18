842c6d65070ebe11557616a7d87b14e8803cc3b8
pcabor <pierre.caboor59@gmail.com>
2024-03-18 09:53:15 +0100

    listpushbackj

--- listpushback.go
+++ listpushback.go
@@ -12,4 +12,16 @@ type List struct {
 
 func ListPushBack(l *List, data interface{}) {
 
+	node := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = node
+		return
+	}
+	current := l.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = node
+
 }
