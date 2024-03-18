62bb4bdefaabc9f5073a5c63825fbf802525027e
pcabor <pierre.caboor59@gmail.com>
2024-03-18 10:34:57 +0100

    listpushfront

---
+++ listpushfront.go
@@ -0,0 +1,22 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	node := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = node
+		return
+	}
+	node.Next = l.Head
+	l.Head = node
+}
