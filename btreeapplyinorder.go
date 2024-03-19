d21ca2492628d7959f28d7b68e23b1a93a189df5
pcabor <pierre.caboor59@gmail.com>
2024-03-19 10:23:48 +0100

    space removed

--- btreeapplyinorder.go
+++ btreeapplyinorder.go
@@ -4,7 +4,7 @@ func BTreeApplyInorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	BTreeApplyInorder(root.Left, f )
+	BTreeApplyInorder(root.Left, f)
 	f(root.Data) 
-	BTreeApplyInorder(root.Right, f )
+	BTreeApplyInorder(root.Right, f)
 }
