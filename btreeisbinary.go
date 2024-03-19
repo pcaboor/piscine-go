4dac7523b02052aefd1b8585fa0db25c0435c065
pcabor <pierre.caboor59@gmail.com>
2024-03-19 13:33:12 +0100

    BTreeIsBinary int removed

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -4,10 +4,7 @@ func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	if root.Left != nil && root.Left.Data.(int) >= root.Data.(int) { // on check si la branche gauche vérifie bien les conditions de ABR que les enfants soient inférieures à la racine
-		return false
-	}
-	if root.Right != nil && root.Right.Data.(int) <= root.Data.(int) { // on check si la branche gauche vérifie bien les conditions de ABR que les enfants soient supèrieures à la racine
+	if (root.Left != nil && root.Left.Data >= root.Data) || (root.Right != nil && root.Right.Data <= root.Data) { // on check si la branche gauche et droite vérifient bien les conditions de ABR que les enfants soient respectivement inférieurs et superieurs  à la racine
 		return false
 	}
 	return BTreeIsBinary(root.Left) && BTreeIsBinary(root.Right)
