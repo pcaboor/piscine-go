a7a1cedb4e3ac1cc6ee67dd81c5fd15049ae762a
pcabor <pierre.caboor59@gmail.com>
2024-03-19 11:41:34 +0100

    uptdate

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -1,15 +1,12 @@
 package piscine
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
-	if root == nil {
-		return nil
-	}
-	if elem == root.Data {
+
+	if root == nil || elem == root.Data {
 		return root
-	} else if elem < root.Data {
-		BTreeSearchItem(root.Left, elem)
-	} else {
-		BTreeSearchItem(root.Right, elem)
+	} 
+	if elem < root.Data {
+		return BTreeSearchItem(root.Left, elem)
 	}
-	return root
+	return BTreeSearchItem(root.Right, elem)
 }
