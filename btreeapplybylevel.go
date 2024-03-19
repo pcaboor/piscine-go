de064ae7ccbf9fdfc7e2ae1c12a6dd1c84647a2e
pcabor <pierre.caboor59@gmail.com>
2024-03-19 14:10:21 +0100

    BTreeApplyByLevel

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -4,7 +4,22 @@ func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
-	BTreeApplyPreorder(root.Left, f)
-	BTreeApplyPreorder(root.Right, f)
+	t := []*TreeNode{root}
+	for len(t) > 0 {
+
+		n := t[0]
+		t = t[1:]
+
+		_, err := f(n.Data)
+		
+		if err != nil {
+			return
+		}
+		if n.Left != nil {
+			t = append(t, n.Left)
+		}
+		if n.Right != nil {
+			t = append(t, n.Right)
+		}
+	}
 }
