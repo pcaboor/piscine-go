98fab6d5011763cc972272524fc076fd6951e6b1
pcabor <pierre.caboor59@gmail.com>
2024-03-19 11:47:10 +0100

    go fmt

--- btreesearchitem.go
+++ btreesearchitem.go
@@ -3,7 +3,7 @@ package piscine
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
 	if root == nil || elem == root.Data {
 		return root
-	} 
+	}
 	if elem < root.Data {
 		return BTreeSearchItem(root.Left, elem)
 	}
