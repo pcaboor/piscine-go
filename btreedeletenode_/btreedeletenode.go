6a457ae5cac4e2240cf5df33e24aa0b499bff73a
pcaboor <pierre.caboor59@gmail.com>
2024-06-03 12:12:34 +0200

    folder added

--- btreedeletenode_/btreedeletenode.go
+++ btreedeletenode_/btreedeletenode.go
@@ -1,47 +1,44 @@
-package piscine 
+package piscine
 
 type TreeNode struct {
-    Data string
-    Left  *TreeNode
-    Right *TreeNode
-    Parent *TreeNode
+	Data   string
+	Left   *TreeNode
+	Right  *TreeNode
+	Parent *TreeNode
 }
 
 func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
-    if root == nil {
-        return nil
-    }
+	if root == nil {
+		return nil
+	}
 
-    if node.Data < root.Data {
-        root.Left = BTreeDeleteNode(root.Left, node)
-    } else if node.Data > root.Data {
-        root.Right = BTreeDeleteNode(root.Right, node)
-    } else {
-     
+	if node.Data < root.Data {
+		root.Left = BTreeDeleteNode(root.Left, node)
+	} else if node.Data > root.Data {
+		root.Right = BTreeDeleteNode(root.Right, node)
+	} else {
 
-       
-        if node.Left == nil && node.Right == nil {
-            root = nil
-        } else if node.Left == nil { 
-            root = root.Right
-        } else if node.Right == nil { 
-            root = root.Left
-        } else { 
-            successor := FindMinNode(node.Right)
-            node.Data = successor.Data
-            node.Right = BTreeDeleteNode(node.Right, successor)
-        }
-    }
+		if node.Left == nil && node.Right == nil {
+			root = nil
+		} else if node.Left == nil {
+			root = root.Right
+		} else if node.Right == nil {
+			root = root.Left
+		} else {
+			successor := FindMinNode(node.Right)
+			node.Data = successor.Data
+			node.Right = BTreeDeleteNode(node.Right, successor)
+		}
+	}
 
-    return root
+	return root
 }
 
-
 func FindMinNode(node *TreeNode) *TreeNode {
-    for node.Left != nil {
-        node = node.Left
-    }
-    return node
+	for node.Left != nil {
+		node = node.Left
+	}
+	return node
 }
 
 func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
